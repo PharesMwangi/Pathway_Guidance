3792b986ea88a904a6bafd4299c20c0248a57d49
PharesMwangi <mwasmwas683@gmail.com>
2026-03-11 10:46:41 +0300

    initial commit

---
+++ react-frontend/src/pages/SignUp.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import { signUp } from "../lib/auth"
+import { supabase } from "../lib/supabaseClient"
+
+export default function Signup() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleSignup(e) {
+  e.preventDefault()
+
+  try {
+    const data = await signUp(email, password)
+
+    const userId = data.user.id
+
+    await supabase.from("students").insert([
+      {
+        user_id: userId
+      }
+    ])
+
+    alert("Signup successful")
+
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+  return (
+    <form onSubmit={handleSignup}>
+      <h2>Signup</h2>
+
+      <input
+        type="email"
+        placeholder="john@school.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Sign Up</button>
+    </form>
+  )
+}
