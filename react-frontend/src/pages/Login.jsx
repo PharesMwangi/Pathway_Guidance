5c157ece7d93720526f3cee70d86ae6caa02e9c2
PharesMwangi <mwasmwas683@gmail.com>
2026-03-11 10:46:02 +0300

    initial commit

---
+++ react-frontend/src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { signIn } from "../lib/auth";
+
+export default function Login(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            await signIn(email, password);
+            alert("Login successful!");
+        }catch(error){
+            alert(error.message);
+        }
+    }
+    return(
+        <form onSubmit={handleLogin}>
+            <h2>LogIn</h2>
+
+            <input 
+                type="email" 
+                placeholsrt="john@school.com"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input 
+                type="password" 
+                placeholsrt="password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Log-In</button>
+        </form>
+    )
+}
