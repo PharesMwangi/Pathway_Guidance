e9d1f144bc8c25052410cc85e16983af2d76ea15
PharesMwangi <mwasmwas683@gmail.com>
2026-03-12 12:05:06 +0300

    modified to test subject creation and academic results

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Link } from "react-router-dom"
-import Signup from "./pages/SignUp"
-import Login from "./pages/Login"
+import Signup from "./pages/auth/SignUp"
+import Login from "./pages/auth/Login"
+import Subjects from "./pages/admin/Subjects"
+import AcademicResults from "./pages/student/AcademicResults"
 
 export default function App() {
   return (
@@ -8,12 +10,16 @@ export default function App() {
 
       <nav>
         <Link to="/signup">Signup</Link> |{" "}
-        <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link> | {" "}
+        <Link to="/admin/subjects">Subjects</Link> |  {" "}
+        <Link to="/academic/academic_scores">Academic </Link>
       </nav>
 
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin/subjects" element={<Subjects />} />
+        <Route path="/academic/academic_scores" element={<AcademicResults />} />
       </Routes>
 
     </div>
