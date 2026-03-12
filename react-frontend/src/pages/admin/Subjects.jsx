5ffbc3b93805649cc32ba8d23ee073cac80e62af
PharesMwangi <mwasmwas683@gmail.com>
2026-03-12 11:59:48 +0300

    initial commit

---
+++ react-frontend/src/pages/admin/Subjects.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabaseClient";
+
+export default function Subjects(){
+    const [subjects, setSubjects] = useState([])
+    const [subjectName, setSubjectName] = useState("")
+
+    useEffect(() =>{
+        fetchSubjects()
+    }, [])
+
+    async function fetchSubjects(){
+        const { data } = await supabase
+        .from ("subjects")
+        .select("*")
+        .order("id", {ascending: true})
+
+        if(!error) {
+            setSubjects(data)
+        }
+    }
+
+    async function addSubject(){
+        if(!subjectName){
+            alert("Please enter a subject name");
+            return;
+        }
+
+        const { error } =await supabase
+        .from ("subjects")
+        .insert ([{ name: subjectName}])
+
+        if (error){
+            alert(error.message)
+        }else{
+            setSubjectName("")
+            fetchSubjects()
+        }
+    }
+
+    async function deleteSubject(id){
+        const { error } = await supabase
+        .from("subjects")
+        .delete()
+        .eq("id", id)
+
+        if(error){
+            alert(error.message)
+        }else{
+            fetchSubjects()
+        }
+    }
+
+    return(
+        <div>
+            <h2>Manage Subjects</h2>
+
+            <input 
+            type="text"
+            placeholder="subject name"
+            value={subjectName}
+            onChange={(e) => setSubjectName(e.target.value)} />
+
+            <button onClick={addSubject}>Add Subject</button>
+
+            <hr />
+
+            <h3>Subjects</h3>
+
+            {subjects.map((subject) => (
+                <div key={subject.id}>
+
+                    {subject.name}
+
+                    <button onClick={() =>deleteSubject(subject.id)}>Delete</button>
+
+                </div>
+            ))}
+        </div>
+    )
+}
