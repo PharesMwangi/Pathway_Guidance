3858e00c61cfcc66d2169c9be308b41ea0f48633
PharesMwangi <mwasmwas683@gmail.com>
2026-03-19 12:34:50 +0300

    used dotenv to import env variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const assessmentRoutes = require('./routes/assessment.routes');
 const resultsRoutes = require('./routes/results.routes');
